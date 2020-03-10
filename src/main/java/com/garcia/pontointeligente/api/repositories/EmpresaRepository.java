3f86f0454ebf03fde0c41d6b48299c730ef48279
Jeferson Garcia <jeferson87.ti@gmail.com>
2020-03-10 09:39:19 -0300

    Criadas interfaces do Repository. Criado profile de Testes. Criada classe para encriptar a senha.

---
+++ src/main/java/com/garcia/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.garcia.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.garcia.pontointeligente.api.domains.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+}
