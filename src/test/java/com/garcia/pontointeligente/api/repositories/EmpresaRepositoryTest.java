3b6d589f204262a5f1d2f4a0f0ff37e26096e4c1
Jeferson Garcia <jeferson87.ti@gmail.com>
2020-03-10 11:48:35 -0300

    Adicionados testes unitários para as classes do pacote repositories.

---
+++ src/test/java/com/garcia/pontointeligente/api/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.garcia.pontointeligente.api.repositories;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.garcia.pontointeligente.api.domains.Empresa;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
+public class EmpresaRepositoryTest {
+	
+	@Autowired
+	private  EmpresaRepository empresaRepository;
+	
+	private static final String CNPJ = "51463645000100";
+
+	@BeforeAll
+	public void setUp() throws Exception {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepository.save(empresa);
+	}
+	
+	@AfterAll
+    public final void tearDown() { 
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarPorCnpj() {
+		Empresa empresa = empresaRepository.findByCnpj(CNPJ);
+		
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
+
+}
